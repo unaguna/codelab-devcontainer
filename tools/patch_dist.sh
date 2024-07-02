4d10bd34e6ec77f7b52b84bfb8d722a3c0796405
K-izumi <k.izumi.ysk@gmail.com>
2024-07-02 05:33:06 +0000

    ✨ run patch_dist.sh automatically

--- tools/patch_dist.sh
+++ tools/patch_dist.sh
@@ -27,11 +27,10 @@ google-codelab #drawer .metadata {
 EOF
 `
 
-# ディレクトリ内のすべてのファイルの変更を監視し、変更があった場合に文字列置換を実行
+# ディレクトリ内の\ファイルの変更を監視し、変更があった場合に文字列置換を実行
+# CAUTION: この変換は何度も実施されることがあるので、冪等になるように注意して作成すること。
 inotifywait -m -e modify --format '%w%f' -r "$DIR_TO_WATCH" | while read -r FILE; do
     if [[ "$FILE" == *.html ]]; then
-        # プロトコルのないリンクを修正
-
         sed -i \
             -e 's|href="//|href="http://|g' \
             -e "s/google-codelab-analytics/!--/g" \
