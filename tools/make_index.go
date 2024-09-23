6913f57bffe5e33ef4247a1d77bd68d2777d03c9
K-izumi <k.izumi.ysk@gmail.com>
2024-09-23 11:37:19 +0000

    ♻ refactor make_index.go

--- tools/make_index.go
+++ tools/make_index.go
@@ -8,16 +8,16 @@ import (
 	"path"
 )
 
-func main() {
-	type Codelab struct {
-		Title string
-		Id    string
-	}
-	type Model struct {
-		Codelabs []Codelab
-	}
+type Codelab struct {
+	Title string
+	Id    string
+}
 
-	codelabs := make([]Codelab, 0, 10)
+type Model struct {
+	Codelabs []Codelab
+}
+
+func main() {
 
 	distDir := "./dist"
 	codelabFiles, err := fs.Glob(os.DirFS(distDir), "**/codelab.json")
@@ -25,34 +25,54 @@ func main() {
 		panic(err)
 	}
 
-	for _, filepath := range codelabFiles {
+	codelabs, err := parseCodelabJsons(codelabFiles, distDir)
+	if err != nil {
+		panic(err)
+	}
+
+	model := Model{Codelabs: codelabs}
+	if err := outputIndexHtml(model, distDir); err != nil {
+		panic(err)
+	}
+}
+
+// 引数に指定した codelab.json ファイルをパースする。
+func parseCodelabJsons(codelabJsonFiles []string, distDir string) ([]Codelab, error) {
+	codelabs := make([]Codelab, 0, 10)
+
+	for _, filepath := range codelabJsonFiles {
 		file, err := os.Open(path.Join(distDir, filepath))
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 
 		var c Codelab
 		if err := json.NewDecoder(file).Decode(&c); err != nil {
-			panic(err)
+			return nil, err
 		}
 
 		codelabs = append(codelabs, c)
 	}
 
-	model := Model{Codelabs: codelabs}
+	return codelabs, nil
+}
 
+// index.html を出力
+func outputIndexHtml(model Model, distDir string) error {
 	tmpl, err := template.ParseFiles("./src/index.html")
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	file, err := os.OpenFile(path.Join(distDir, "index.html"), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	err = tmpl.Execute(file, model)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	return nil
 }
